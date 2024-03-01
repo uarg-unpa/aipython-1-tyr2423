fd0634041d528b3ee7fce0e4a3363bb5a0b3a8bd
tyr2423 <62185996+tyr2423@users.noreply.github.com>
2024-03-01 19:05:32 +0000

    ultimo3

--- entrega/principal.py
+++ entrega/principal.py
@@ -1,4 +1,9 @@
 print("Taller AiPython")
+
+#argumento posicional
 print("Hola", "Soy", "Jose")
-print("hola soy jose", sep="", end="\n")
-print()
+#argumentos sep y end
+print("hola", "soy", "jose", sep="*", end="\n")
+print("hola", "soy", "jose", end="+")
+print("soy el que sigue")
+print("soy el que no sigue")
