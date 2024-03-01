02dacd76a902042ce9d6235862ef464b50a40a94
tyr2423 <62185996+tyr2423@users.noreply.github.com>
2024-03-01 19:28:13 +0000

    ultimo4

---
+++ entrega/guia/ejercicios/ejercicio1e.py
@@ -0,0 +1,3 @@
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano")
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que es un humano')
+print("Una computadora puede ser llamada 'inteligente' si logra engañar a una persona haciéndole creer que es un humano")
