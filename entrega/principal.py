1e57d82f5a57600488a54848f5d89fff168f0cbe
tyr2423 <62185996+tyr2423@users.noreply.github.com>
2024-03-05 18:42:21 +0000

    ultimo6

--- entrega/principal.py
+++ entrega/principal.py
@@ -1,9 +1,51 @@
-print("Taller AiPython")
+# print("Taller AiPython")
 
-#argumento posicional
-print("Hola", "Soy", "Jose")
-#argumentos sep y end
-print("hola", "soy", "jose", sep="*", end="\n")
-print("hola", "soy", "jose", end="+")
-print("soy el que sigue")
-print("soy el que no sigue")
+# #argumento posicional
+# print("Hola", "Soy", "Jose")
+# #argumentos sep y end
+# print("hola", "soy", "jose", sep="*", end="\n")
+# print("hola", "soy", "jose", end="+")
+# print("soy el que sigue")
+# print("soy el que no sigue")
+
+# # argumentos posicionales
+
+# print(10, 3.14, "cadena", True)
+
+# edad=int(input("ingrese su edad:"))
+
+# # print ("su edad es"+edad) mal
+# print("su edad es", edad)
+# print("su edad es"+str(edad))
+
+# edad2=18
+# print(F"su edad es {edad2}")
+
+# num1=3
+# num2=4
+# print(f"{num1}+{num2}={num1+num2}")
+# print(f"{num1}-{num2}={num1-num2}")
+# print(f"{num1}*{num2}={num1*num2}")
+# print(f"{num1}%{num2}={num1%num2}")
+# print(f"{num1}/{num2}={num1/num2}")
+# print(f"{num1}//{num2}={num1//num2}")
+# print(f"{num1}**{num2}={num1**num2}")
+
+
+text="EsTo eS uN texTo MeZclAdO"
+
+# title
+print(text.title()) #ordena el txt poniendo iniciales en mayus
+print(text)
+
+texto=text.title()
+print(texto)
+
+# upper and lower
+print(texto.upper())
+print(texto.lower())
+
+# replace
+
+print(texto.replace(" ", "-"))
+
